36f2b83794184cb96d276a1a9a0e285aac63ce5f
Sabhariaswath <35764741+Sabhariaswath@users.noreply.github.com>
2018-03-07 16:27:52 +0530

    Create sri-45.c

---
+++ sri-45.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+char a[50];
+int i,count=0;
+printf("enter the string:");
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+if(a[i]>='0' && a[i]<='9')
+{
+count=count+1;
+}
+}
+printf("\n%d",count);
+return 0;
+}
